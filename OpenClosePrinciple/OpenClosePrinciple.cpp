c0ca82b0ab96a8792293d8aac43912f65a7b305d
nhile <nhile@DESKTOP-NT33GLC>
2022-09-01 16:09:38 +0300

    Add project files.

---
+++ OpenClosePrinciple/OpenClosePrinciple.cpp
@@ -0,0 +1,243 @@
+#include <iostream>
+#include <vector>
+
+enum EColor{ eRed, eGreen, eBlue };
+enum ESize{ eS, eM, eL, eXL };
+
+std::string EColorToStirng(EColor color)
+{
+    std::string result;
+    switch(color)
+    {
+    case EColor::eRed:
+        result = "Red";
+        break;
+    case EColor::eGreen:
+        result = "Green";
+        break;
+    case EColor::eBlue:
+        result = "Blue";
+        break;
+    default:
+        break;
+    }
+    return result;
+}
+
+std::string ESizeToStirng(ESize size)
+{
+    std::string result;
+    switch (size)
+    {
+    case ESize::eS:
+        result = "Small";
+        break;
+    case ESize::eM:
+        result = "Medium";
+        break;
+    case ESize::eL:
+        result = "Large";
+        break;
+    case ESize::eXL:
+        result = "Xtra Large";
+        break;
+    default:
+        break;
+    }
+    return result;
+}
+
+class Product
+{
+public:
+    Product(const std::string& type, EColor color, ESize size) 
+        : fType(type)
+    {
+        fColor = color;
+        fSize = size;
+    }
+
+    std::string GetType() { return fType; }
+    EColor      GetColor() { return fColor; }
+    ESize       GetSize() { return fSize; }
+
+private:
+    std::string fType;
+    EColor      fColor;
+    ESize       fSize;
+};
+
+template <typename T>
+class MultipleSpecifications;
+
+template <typename T>
+class Specification
+{
+public:
+    virtual bool IsSatisfied(T item) = 0;
+
+    MultipleSpecifications<T> operator&& (Specification <T>& other)
+    {
+        return MultipleSpecifications<T>(*this, other);
+    }
+};
+
+class SpecByColor : public Specification<Product>
+{
+public:
+    SpecByColor(EColor color) { fColor = color; }
+
+    virtual bool IsSatisfied(Product item) override
+    {
+        return item.GetColor() == fColor;
+    }
+
+private:
+    EColor fColor;
+};
+
+class SpecBySize : public Specification<Product>
+{
+public:
+    SpecBySize(ESize size) { fSize = size; }
+
+    virtual bool IsSatisfied(Product item) override
+    {
+        return item.GetSize() == fSize;
+    }
+
+private:
+    ESize fSize;
+};
+
+class SpecByType: public Specification<Product>
+{
+public:
+    SpecByType(const std::string& type) { fType = type; }
+
+    virtual bool IsSatisfied(Product item) override
+    {
+        return item.GetType() == fType;
+    }
+
+private:
+    std::string fType;
+};
+
+template <typename T>
+class MultipleSpecifications : public Specification<T>
+{
+public:
+    MultipleSpecifications(Specification<T>& first, Specification<T>& second) 
+        : fFirst(first)
+        , fSecond(second)
+    {}
+
+    virtual bool IsSatisfied(T item) override
+    {
+        return fFirst.IsSatisfied(item) && fSecond.IsSatisfied(item);
+    }
+
+private:
+    Specification<T>& fFirst;
+    Specification<T>& fSecond;
+};
+
+template <typename T>
+class Filter
+{
+    virtual std::vector<T*> FilterIt(std::vector<T*> items, Specification<T>& spec) = 0;
+    virtual std::vector<T*> FilterIt(std::vector<T*> items, MultipleSpecifications<T>& spec) = 0;
+};
+
+class ProductFilter : Filter<Product>
+{
+public:
+    virtual std::vector<Product*> FilterIt(std::vector<Product*> items, Specification<Product>& spec) override
+    {
+        std::vector<Product*> filteredItems;
+        for (auto& item : items)
+        {
+            if (spec.IsSatisfied(*item))
+            {
+                filteredItems.push_back(item);
+            }
+        }
+
+        return filteredItems;
+    }
+
+    virtual std::vector<Product*> FilterIt(std::vector<Product*> items, MultipleSpecifications<Product>& spec) override
+    {
+        std::vector<Product*> filteredItems;
+        for (auto& item : items)
+        {
+            if (spec.IsSatisfied(*item))
+            {
+                filteredItems.push_back(item);
+            }
+        }
+
+        return filteredItems;
+    }
+};
+
+int main()
+{
+    Product greenShirtS("Shirt", eGreen, eS);
+    Product greenShirtM("Shirt", eGreen, eM);
+    Product greenShirtL("Shirt", eGreen, eL);
+
+    Product bluePantsL("Pants", eBlue, eL);
+    Product bluePantsXL("Pants", eBlue, eXL);
+
+    Product blueShirtM("Shirt", eBlue, eM);
+    Product blueShirtL("Shirt", eBlue, eL);
+
+    std::vector<Product*> products;
+    products.push_back(&greenShirtS);
+    products.push_back(&greenShirtM);
+    products.push_back(&greenShirtL);
+
+    products.push_back(&bluePantsL);
+    products.push_back(&bluePantsXL);
+
+    products.push_back(&blueShirtM);
+    products.push_back(&blueShirtL);
+
+
+    SpecByColor byColor(EColor::eGreen);
+    ProductFilter filter;
+    std::vector<Product*>greenProducts = filter.FilterIt(products, byColor);
+
+    SpecBySize bySize(ESize::eL);
+
+    SpecByType byType("Pants");
+
+    std::cout << "Filtering green products:" << std::endl;
+    for (const auto& item : greenProducts)
+    {
+        std::cout << item->GetType() << " " << ESizeToStirng(item->GetSize()) << " " << EColorToStirng(item->GetColor()) << std::endl;
+    }
+    std::cout << std::endl;
+
+    std::vector<Product*>shirts = filter.FilterIt(products, byType);
+
+    std::cout << "Filtering pants:" << std::endl;
+    for (const auto& item : shirts)
+    {
+        std::cout << item->GetType() << " " << ESizeToStirng(item->GetSize()) << " " << EColorToStirng(item->GetColor()) << std::endl;
+    }
+    std::cout << std::endl;
+
+    auto mult = bySize && byType;
+
+    std::vector<Product*> shirtsSizeL = filter.FilterIt(products, mult);
+
+    std::cout << "Filtering large pants:" << std::endl;
+    for (const auto& item : shirtsSizeL)
+    {
+        std::cout << item->GetType() << " " << ESizeToStirng(item->GetSize()) << " " << EColorToStirng(item->GetColor()) << std::endl;
+    }
+}
+  
